b148dc4a399974ae42376dd1930a59a5c93e84bc
ivanarsov2003 <arsovivan38@gmail.com>
2023-12-09 13:04:46 +0100

    BasicJs-ZodiacHomework

---
+++ Basic JS Homework 02/code/src/app.js
@@ -0,0 +1,45 @@
+alert("Click \"ok \" then write your year to see what zodiac you are.");
+let year = parseInt(prompt("Your year here"));
+let zodiac = (year - 4) % 12;
+// console log
+//alert
+if(zodiac === 0){
+    alert("Your zodiac is: RAT");
+    console.log("Your zodiac is: RAT");
+}else if(zodiac === 1){
+    alert("Your zodiac is: Ox");
+    console.log("Your zodiac is: Ox");
+}else if(zodiac === 2){
+   alert("Your zodiac is: Tiger");
+   console.log("Your zodiac is: Tiger");
+}else if(zodiac === 3){
+    alert("Your zodiac is: Rabbit");
+    console.log("Your zodiac is: Rabbit");
+}else if(zodiac === 4){
+    alert("Your zodiac is: Dragon");
+    console.log("Your zodiac is: Dragon");
+}else if(zodiac === 5){
+    alert("Your zodiac is: Snake");
+    console.log("Your zodiac is: Snake");
+}else if(zodiac === 6){
+    alert("Your zodiac is: Horse");
+    console.log("Your zodiac is: Horse");
+}else if(zodiac === 7){
+    alert("Your zodiac is: Goat");
+    console.log("Your zodiac is: Goat");
+}else if(zodiac === 8){
+    alert("Your zodiac is: Monkey");
+    console.log("Your zodiac is: Monkey");
+}else if(zodiac === 9){
+    alert("Your zodiac is: Rooster");
+    console.log("Your zodiac is: Rooster");
+}else if(zodiac === 10){
+    alert("Your zodiac is Dog");
+    console.log("Your zodiac is Dog");
+}else if(zodiac ===11){
+    alert("Your zodiac is: Pig");
+    console.log("Your zodiac is: Pig")
+}
+
+
+
